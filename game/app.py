fab4d720f10f4413a4ef1b25a4516c5f74782da8
yotamitai <yotamitai@gmail.com>
2023-08-31 13:01:25 +0300

    fixed points accumulation

--- game/app.py
+++ game/app.py
@@ -11,25 +11,30 @@ app.secret_key = "M2vYPG#Lf2rSMWs$!Y"
 # Initialize points
 points = 0
 
+
 @app.route("/", methods=["GET", "POST"])
 def game():
-    session["points"] = 0
-    session["max_points"] = 0
+    if session.get("points") is None:
+        session["points"] = 0
+    if session.get("max_points") is None:
+        session["max_points"] = 0
 
     if request.method == "POST":
-        choice = bool(request.form["choice"]) #TODO: change points based on choice
+        choice = bool(request.form["choice"])  # TODO: change points based on choice
         scenario = get_scenario(test_scenario=True)
 
         # Update points based on the choice
-        session["points"] += choice #TODO: edit points
+        session["points"] += choice  # TODO: edit points
         session["max_points"] = max(session["max_points"], session["points"])
 
-        return render_template("game.html", scenario=scenario, points=points)
-    return render_template("game.html", scenario=get_scenario(test_scenario=True), points=points)
+        return render_template("game.html", scenario=scenario, points=session["points"])
+    return render_template("game.html", scenario=get_scenario(test_scenario=True), points=session["points"])
+
 
 @app.template_filter("enumerate")
 def jinja2_enumerate(iterable, start=0):
     return enumerate(iterable, start=start)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
